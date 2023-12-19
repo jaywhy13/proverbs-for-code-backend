b9d7b80d6387d370941654586f727035a0cc66e7
Jean-Mark Wright <jeanmark.wright@gmail.com>
2023-12-18 22:17:20 -0500

    Added support for Typescript and added dotenv
    In order to support Typescript we needed to do a few
    things:
    - Install Typescript
    - Install types for NodeJS and Express
    - Added Typescript compiler configuration (tsconfig.json)
      - Modified the configuration to spit out compiled
        files to ./dist instead of the main directory
    - Added ts-node to provide an environment for running
      Typescript files. NodeJS can't run Typescript files
      directly.
    - Added nodemon for detecting file changes and re-running
     the script. It detects and works with ts-node.
    - Change the index.js file to a index.ts and update
      the syntax (e.g. replacing require with import,
      add type annotation)
    - Added some scripts to package.json to be able to
      run it.
      - build - this runs the Typescript compiler (tsc)
        and outputs the JS files in the dist directory.
      - dev - runs nodemon with ts-node for detecting
        changes and re-running files.
      - start - this runs NodeJS on the dist/index.js
        file
    https://blog.logrocket.com/how-to-set-up-node-typescript-express/

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from "express"
+import dotenv from "dotenv"
+
+
+dotenv.config()
+
+
+const app: Express = express();
+const PORT = process.env.PORT;
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express & Typescript server');
+})
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at localhost:${PORT}`)
+})
