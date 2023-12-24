960931d0cb6e8988afde56cf5c9d90763000850b
Jean-Mark Wright <jeanmark.wright@gmail.com>
2023-12-23 22:46:15 -0500

    Added a handler for POST suggestions
    In this commit, I'm adding a handler for getting
    requests for suggestions and calling the service
    to get the suggestions and return it.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express, { Express, Request, Response } from "express"
 import dotenv from "dotenv"
+import { SuggestionService } from "./services/suggestions";
+import suggestionService from "./services/llm_suggestion";
+import { Suggestion } from "./constants";
 
 
 dotenv.config()
@@ -11,6 +14,23 @@ const PORT = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express & Typescript server');
+})
+
+app.post('/suggestions', async (req, res) => {
+  console.log("Got a request for suggestions", req)
+  const lesson = req.body.lesson;
+  const numberOfProverbs = 5;
+  const excludeSuggestions: Suggestion[] = [];
+
+  const suggestions = await suggestionService.getSuggestions({ lesson, numberOfProverbs, excludeSuggestions })
+
+  const remoteSuggestions = suggestions.map(suggestion => ({
+    id: suggestion.id,
+    proverb: suggestion.proverb,
+    relation: suggestion.relation
+  }))
+
+  res.json({ "suggestions": remoteSuggestions });
 })
 
 app.listen(PORT, () => {
